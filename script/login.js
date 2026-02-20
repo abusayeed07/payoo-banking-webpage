4ec0a949c462332829141d64fed9d4771237c2b3
abusayeed07 <mdabusayeed133@gmail.com>
2026-02-20 16:00:36 +0600

    Changing login.js

--- script/login.js
+++ script/login.js
@@ -1,23 +1,17 @@
 document.getElementById('login-btn').addEventListener('click', function () {
-    //step-1: get the mobile number input
-    const inputNumber = document.getElementById('input-number')
-    const contactNumber = inputNumber.value;
-    console.log(contactNumber);
-    //step-2: get the pin input
-    const inputPin = document.getElementById('input-pin');
-    const pin = inputPin.value;
-    console.log(pin);
-    //step-3: match pin & mobile number
-    if (contactNumber == '01234567890' && pin == '1234') {
-        //step-3.1: true:::>> alert > homepage
+    const inputNumber = getValueFromInput("input-number");
+    if (inputNumber.length !== 11 || isNaN(inputNumber)) {
+        alert("Invalid account number");
+        return;
+    }
+
+    const inputPin = getValueFromInput("input-pin");
+    if (inputNumber == '01234567890' && inputPin == '1234') {
         alert('Login successfull');
-
         // to link home page 
-        window.location.assign('/home.html')
-
+        window.location.assign('/home.html');
     } else {
-        //step-3.2: false:::>> alert > return
         alert('Login failed');
         return;
     }
-})
+});
